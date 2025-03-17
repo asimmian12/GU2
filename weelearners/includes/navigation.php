d6d247fa34cbf50277c3179b6f7c2132cc0eb8c0
asim mian <asim.mian7878@gmail.com>
2025-03-17 08:18:24 +0000

    l

--- weelearners/includes/navigation.php
+++ weelearners/includes/navigation.php
@@ -8,7 +8,7 @@
         <?php if (!isset($_SESSION['loggedin'])) : ?>
             <!-- For non-logged-in users -->
             <li><a href="<?= ROOT_DIR ?>">Home</a></li>
-            <li><a href="<?= ROOT_DIR ?>vinyl">Vinyl</a></li>
+            <li><a href="<?= ROOT_DIR ?>vinyl">Badge</a></li>
             <li><a href="<?= ROOT_DIR ?>login">Login</a></li>
             <li><a href="<?= ROOT_DIR ?>register">Register</a></li>
         <?php else: ?>
