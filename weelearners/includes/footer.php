866d2f2fff0406cbc99c8cbdbd492e633b39b903
asim mian <asim.mian7878@gmail.com>
2025-03-24 20:38:17 +0000

    #

--- weelearners/includes/footer.php
+++ weelearners/includes/footer.php
@@ -1,4 +1,26 @@
-<p class="paragraph-copyright"> ©️ Wee Learners 2025</p>
+<footer class="paragraph-copyright">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Wee Learners <?= date('Y') ?></span>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <p>Social Media Links</p>
+                <li>
+                    <a href="https://www.facebook.com/" class="mr-4 hover:underline md:mr-6 ">Facebook</a>
+                </li>
+                <li>
+                    <a href="https://x.com/" class="mr-4 hover:underline md:mr-6">X</a>
+                </li>
+                <li>
+                    <a href="https://instagram.com/" class="hover:underline">Instagram</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; Wee Learners. <?= date('Y') ?> All Rights Reserved.</span>
+    </div>
+</footer>
+
+
 <script src="assets/js/script.js"></script>
 </body>
 </html>
