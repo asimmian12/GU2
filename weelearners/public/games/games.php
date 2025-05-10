aa2a17ad7b4ec070724fae91cf015fb1bec264c7
asim mian <asim.mian7878@gmail.com>
2025-05-10 02:17:21 +0100

    Refactor games.php to improve code readability by removing unnecessary whitespace

--- weelearners/public/games/games.php
+++ weelearners/public/games/games.php
@@ -6,19 +6,12 @@ include '../../includes/header.php';
 <?php
 
 // The function fetchFromApi is used to retrieve game data from the FreeToGame API
-
 function fetchFromApi($url) {
-
     // The URL is hardcoded to fetch PC games data from the API
-
     $url = "https://www.freetogame.com/api/games?platform=pc";
-
     // The file_get_contents function is used to send a GET request to the API and fetch the response
-   
     $response = file_get_contents($url);
-
     // The JSON response is decoded into an associative array and returned
-
     return json_decode($response, true);
 }
 
@@ -26,18 +19,12 @@ function fetchFromApi($url) {
   $url = "https://www.freetogame.com/api/games?platform=pc";
   // The fetchFromApi function is called to get the games data and store it in the $games variable
   $games = fetchFromApi($url);
-
-
-
 ?>
-
-    <h1 class="h2-secondary-colour">Games</h1>
-    
+    <h1 class="h2-secondary-colour">Games</h1>   
     <?php
     shuffle($games);
     $games = array_slice($games, 0, 3);
     ?>
-
     <section>
         <?php foreach ($games as $game): ?>
             <div>
