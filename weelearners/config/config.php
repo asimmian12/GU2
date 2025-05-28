5af691fa53cbf94cf733a1f99eab4ce0d2ac9b93
asim mian <asim.mian7878@gmail.com>
2025-05-28 18:44:03 +0100

    Refactor database connection code to use arrays for credentials; improve structure and readability

--- weelearners/config/config.php
+++ weelearners/config/config.php
@@ -21,3 +21,40 @@ try {
 // }
 
 ?>
+
+<!-- <//?php
+// $hn = "localhost";
+// $un = "admin";
+// $pw = "password";
+// $db = "learners_db";
+
+$un = array(
+    "admin" => "password"
+);
+
+$pw = array(
+    "password" => "password"
+);
+
+$hn = array(
+    "localhost" => "localhost"
+);
+
+$db = array(
+    "learners_db" => "localhost"
+);
+
+$conn = new mysqli($hn['localhost'], $un['admin'], $pw['password'], $db['learners_db']);
+
+try {
+    if ($conn->connect_error) {
+        throw new Error("Connection failed: " . $conn->connect_error);
+    }
+} catch (Error) {
+    exit();
+}
+
+// else{
+//     echo "Connection Succuessfull";
+// }
+?> -->
